e31ce1025cfcc27f9cd979ffb4d2468f3bc54b8e
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-16 01:37:12 -0800

    update: logo path in backend dir

--- frontend/src/features/Header.tsx
+++ frontend/src/features/Header.tsx
@@ -1,16 +1,13 @@
 import { NavLink } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
+import logo from "../../public/logo.svg";
 
 const Header: React.FC = () => {
   return (
     <div className="fixed z-10 w-screen">
       <nav className="flex items-center justify-around h-12 bg-opacity-0 backdrop-blur-md">
         <NavLink to={`/`}>
-          <img
-            src="/public/logo.svg"
-            alt="movie-picker-logo"
-            className="w-auto h-10"
-          />
+          <img src={logo} alt="movie-picker-logo" className="w-auto h-10" />
         </NavLink>
         <SearchBar />
         <NavLink to={`/about`} className="text-orange-500">
