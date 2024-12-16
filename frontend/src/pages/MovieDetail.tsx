100f31a133586a414d9644897ab82593773a1492
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-16 01:31:42 -0800

    update: remove unnecessary log and change icon path

--- frontend/src/pages/MovieDetail.tsx
+++ frontend/src/pages/MovieDetail.tsx
@@ -41,7 +41,6 @@ const MovieDetail = () => {
         const { data } = await axios.get(
           `https://movie-picker-backend.vercel.app/movies/${id}`
         );
-        console.log(data);
         setMovieDetail(data);
         setMovieCredits(data.credits);
         setMovieReviews(data.reviews?.results);
